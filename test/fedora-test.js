b8bab0a7959ebe64ec1adcca846183ce9efacf27
Jimmy Tang <jtang@tchpc.tcd.ie>
2012-03-27 23:03:45 +0100

    xml output isn't checked to see if its defined.

--- test/fedora-test.js
+++ test/fedora-test.js
@@ -4,10 +4,12 @@ var fedora = require('fedora');
 module.exports = {
 	'test1 GetFedoraList': function(beforeExit, assert) {
 		xml = fedora.getFedoraList();
+		assert.isDefined(xml);
 		assert.isNotNull(xml);
 	},
 	'test2 GetFedoraItem': function(beforeExit, assert) {
 		xml = fedora.getFedoraObject();
+		assert.isDefined(xml);
 		assert.isNotNull(xml);
 	},
 	'test3 GetNextPID': function(beforeExit, assert) {
