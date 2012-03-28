3f4f068b9208779e1220b784ad207aa9072af219
Quirijn Groot Bluemink <Quirijn.Groot.Bluemink@howest.be>
2012-03-28 09:44:27 +0100

    stash changes

--- test/fedora-test.js
+++ test/fedora-test.js
@@ -1,20 +1,20 @@
 var assert = require('assert');
-var fedora = require('fedora');
+var fedora = require('../lib/fedora');
 
 module.exports = {
 	'test1 GetFedoraList': function(beforeExit, assert) {
 		xml = fedora.getFedoraList();
-		assert.isDefined(xml);
 		assert.isNotNull(xml);
 	},
 	'test2 GetFedoraItem': function(beforeExit, assert) {
-		xml = fedora.getFedoraObject();
-		assert.isDefined(xml);
-		assert.isNotNull(xml);
+		fedora.getFedoraObject("dri:100", function(resultData){
+			
+			assert.includes(resultData,"dri:100");
+		});
 	},
 	'test3 GetNextPID': function(beforeExit, assert) {
 		
-		fedora.createFedoraObject("node", function(result){
+		fedora.getNextPID("node", function(result){
 			assert.includes(result,"node:");
 		});
 	}
