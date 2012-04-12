fd8495e531d2154f4ec08260fd71e808c974cb31
Jimmy Tang <jtang@tchpc.tcd.ie>
2012-04-12 11:50:55 +0100

    microformatting to make generated docs nicer to read

--- test/serial-fedora.js
+++ test/serial-fedora.js
@@ -7,9 +7,9 @@ var testLabel = "A very nice test label";
 var testResult = "";
 
 describe('Test cases for the node-fedora package', function() {
-	describe('createFedoraObject, will create a FedoraObject', function() {
-		it('should create a FedoraObject', function(done) {
-			fedora.createFedoraObject(testNamespace,testLabel, function(result){
+	describe('Calling createFedoraObject(), will create a fedoraObject', function() {
+		it('should create a fedoraObject', function(done) {
+			fedora.createFedoraObject(testNamespace, testLabel, function(result){
 				testResult = result;
 				result.should.exist;
 				result.should.include(testNamespace + ":");
@@ -18,9 +18,9 @@ describe('Test cases for the node-fedora package', function() {
 		});
 	}),
 
-	describe('getFedoraList, Get a list of FedoraObjects', function() {
-		it('should return a list of FedoraObjects from the fedora repository', function() {
-			fedora.getFedoraList("*",function(resultData){
+	describe('Calling getFedoraList(), Get a list of fedoraObjects', function() {
+		it('should return a list of fedoraObjects from the fedora repository', function() {
+			fedora.getFedoraList("*", function(resultData){
 				resultData.should.exist;
 				resultData.should.include('result');
 				resultData.should.not.be.empty;
@@ -31,7 +31,7 @@ describe('Test cases for the node-fedora package', function() {
 		})
 	}),
 
-	describe('getFedoraObject, get a fedoraObject', function() {
+	describe('Calling getFedoraObject(), get a fedoraObject', function() {
 		it('should return a fedoraObject', function(done) {
 			fedora.getFedoraObject(testResult, function(resultData){
 				resultData.should.include(testResult);
@@ -40,7 +40,7 @@ describe('Test cases for the node-fedora package', function() {
 		});
 	}),
 
-	describe('deleteObject, will delete an object from fedora', function() {
+	describe('Calling deleteObject(), will delete an object from fedora', function() {
 		it('should delete the requested object from fedora', function(done) {
 			fedora.deleteObject(testResult, function(resultData){
 				var myregexp = new RegExp("[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.?[0-9]*Z");
@@ -52,7 +52,7 @@ describe('Test cases for the node-fedora package', function() {
 		})
 	}),
 
-	describe('getNextPID, will get the next PID that is available from fedora', function() {
+	describe('Calling getNextPID(), will get the next PID that is available from fedora', function() {
 		it('should return the next PID from fedora', function(done) {
 			fedora.getNextPID("node", function(resultData){
 				resultData.should.include("node:");
